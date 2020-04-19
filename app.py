ae68f221f8eb5ced9448f1ac32fedb8dd6193d4c
mtfaye <tahafaye@hotmail.com>
2020-04-19 00:49:07 -0400

    graph and callbacks

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import dash
-import dash_table
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output
-from utils import data
+import plotly.graph_objs as go
+from utils import dict_main
+
+data = list(dict_main.keys())
+channels = dict_main[data[0]]
 
 app = dash.Dash(__name__)
 app.title = 'Diabetes Monitoring App'
@@ -13,12 +15,11 @@ app.colors = {'background': '#5F5958'}
 external_stylesheets = ['https://codepen.io/mtfaye/pen/MWgpoyp.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-
 app.layout = html.Div(
     [
         html.Div(
             [
-                html.H1(children='kimaera-dynamics',
+                html.H1(children='Insuline Tracker App',
                         style={'color': 'white', 'fontSize': 23, 'text-indent': 10, 'line-height': 50},
                         className='banner'
                         )
@@ -29,13 +30,39 @@ app.layout = html.Div(
                 children=""" About: """
             )
                 , style={'textAlign': 'left'}),
-            html.P("""Insuline and Glucose Monitoring""")
+            html.P(
+                """ Insulin Game Changer  """
+            )
         ]),
         html.Div(children=[
-            html.P('Select a channel - '),
+            html.Div([
+                dcc.Graph(id='Main-Graph',
+                          figure=go.Figure(
+                              data=[go.Scatter(x=data, y=channels)]
+                          )),
+                html.Div([
+                    dcc.Dropdown(
+                        id='data-dropdown',
+                        options=[{'label': label, 'value': label} for label in data],
+                        value=list(dict_main.keys())[0],
+                        multi=False,
+                        searchable=False)],
+                    style={'width': '33%', 'display': 'inline-block'}),
+                html.Div([
+                    dcc.Dropdown(
+                        id='x-axis-dropdown',
+                        multi=False)],
+                    style={'width': '33%', 'display': 'inline-block'}),
+                html.Div([
+                    dcc.Dropdown(
+                        id='y-axis-dropdown',
+                        multi=False)],
+                    style={'width': '33%', 'display': 'inline-block'})
+            ]),
             html.Div(
                 [
-                    html.P('Hackathon Together vs COVID19 - Developed by team kimaera-dynamics', style={'display': 'inline'}),
+                    html.P('Developed by Team Insuline Game Changer ',
+                           style={'display': 'inline'}),
                     html.A('tahafaye@hotmail.com', href='mailto: tahafaye@hotmail.com')
                 ], className="twelve columns",
                 style={'fontSize': 14, 'padding-top': 18}
@@ -43,6 +70,38 @@ app.layout = html.Div(
         ], className="row")
     ], className='ten columns offset-by-one'
 )
+
+@app.callback(
+    dash.dependencies.Output('Main-Graph', 'figure'),
+    [dash.dependencies.Input('data-dropdown', 'value'),
+     dash.dependencies.Input('x-axis-dropdown', 'value'),
+     dash.dependencies.Input('y-axis-dropdown', 'value')],
+    [dash.dependencies.State('Main-Graph', 'figure')]
+)
+def updateGraph(df_name, x_field, y_field, data):
+    source = data['data']
+    df = dict_main[df_name]
+
+    if x_field and y_field and x_field in df.columns and y_field in df.columns:
+        new_source = [{'x': df[x_field].tolist(), 'y': df[y_field].tolist()}]
+        source = new_source
+    return {
+        'data': source,
+        'layout': data['layout']
+    }
+
+
+@app.callback(
+    [dash.dependencies.Output('x-axis-dropdown', 'options'),
+     dash.dependencies.Output('y-axis-dropdown', 'options')],
+    [dash.dependencies.Input('data-dropdown', 'value')]
+)
+def update_date_dropdown(selected):
+    if selected is None:
+        return dict_main['data-01']
+    fields = [{'label': i, 'value': i} for i in dict_main[selected]]
+    return [fields, fields]
+
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,7 +1,53 @@
 """ Utility functions. """
+import os
+import pandas as pd
 
 
-def data(dataframe):
-    """Get data"""
-    return dataframe
+def dict_main():
+    """ Collects data for each patient and concat on dict"""
+    ddict = {}
+    for file in os.listdir('patient-data'):
+        if file.startswith('data'):
+            name = os.path.splitext(file)[0]
+            ddict[name] = pd.read_csv(os.path.join('patient-data', file), sep='\t')
+            ddict[name].columns = ['Date', 'Time', 'Code', 'Value']
+    return ddict
 
+
+dict_main = dict_main()
+
+"""
+Code Descriptions
+
+33 = Regular insulin dose
+34 = NPH insulin dose
+35 = UltraLente insulin dose
+48 = Unspecified blood glucose measurement
+57 = Unspecified blood glucose measurement
+58 = Pre-breakfast blood glucose measurement
+59 = Post-breakfast blood glucose measurement
+60 = Pre-lunch blood glucose measurement
+61 = Post-lunch blood glucose measurement
+62 = Pre-supper blood glucose measurement
+63 = Post-supper blood glucose measurement
+64 = Pre-snack blood glucose measurement
+65 = Hypoglycemic symptoms
+66 = Typical meal ingestion
+67 = More-than-usual meal ingestion
+68 = Less-than-usual meal ingestion
+69 = Typical exercise activity
+70 = More-than-usual exercise activity
+71 = Less-than-usual exercise activity
+72 = Unspecified special event
+
+"""
+
+
+def treshold():
+    """ Graphs """
+    pass
+
+
+def notifications():
+    """ Return alerts"""
+    pass
